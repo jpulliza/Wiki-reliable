09aabf949946e959b258603a1fae827b83d9eeab
Jonathan Pulliza <jpulliza@gmail.com>
2013-11-17 03:30:00 -0500

    Lots of tries at using JSON.

--- popup.js
+++ popup.js
@@ -1,83 +1,47 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//var obj
 
-/**
- * Global variable containing the query we'd like to pass to Flickr. In this
- * case, kittens!
- *
- * @type {string}
- */
-var QUERY = 'puppies';
+//function callWikipediaAPI(wikipediaPage) {
+    // http://www.mediawiki.org/wiki/API:Parsing_wikitext#parse
+//$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?', { page: wikipediaPage, prop: 'text|images', uselang: 'en' });
+//var json = $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?', { page: "Millard_Fillmore", prop: 'text|images', uselang: 'en' })
 
-var kittenGenerator = {
-  /**
-   * Flickr URL that will give us lots and lots of whatever we're looking for.
-   *
-   * See http://www.flickr.com/services/api/flickr.photos.search.html for
-   * details about the construction of this URL.
-   *
-   * @type {string}
-   * @private
-   */
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-      'method=flickr.photos.search&' +
-      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-      'text=' + encodeURIComponent(QUERY) + '&' +
-      'safe_search=1&' +
-      'content_type=1&' +
-      'sort=interestingness-desc&' +
-      'per_page=20',
+//$.parseJSON(obj)
+    //alert(obj.pageid)
+//}
 
-  /**
-   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
-   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
-   *
-   * @public
-   */
-  requestKittens: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
-  },
+//JSON.parse(obj);
 
-  /**
-   * Handle the 'onload' event of our kitten XHR request, generated in
-   * 'requestKittens', by generating 'img' elements, and stuffing them into
-   * the document for display.
-   *
-   * @param {ProgressEvent} e The XHR ProgressEvent.
-   * @private
-   */
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
-    }
-  },
+//callWikipediaAPI('Millard_Fillmore');
 
-  /**
-   * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlKittenl
-   *
-   * @param {DOMElement} A kitten.
-   * @return {string} The kitten's URL.
-   * @private
-   */
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
-  }
-};
+//var json = '{"result":true,"count":1}',
+    //obj = JSON.parse(json);
 
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-  kittenGenerator.requestKittens();
-});
+//alert(obj.pageid)
+
+//$(document).ready(function () {
+  //  $("button").click(function () {
+    //    $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?', { page: "Millard_Fillmore", prop: 'text|images', uselang: 'en' }, function (result) {
+      //      $.each(result, function (i, field) {
+        //        $("div").append(field + " ");
+          //  });
+//        });
+//    });
+//});
+
+//$(document).ready(function () {
+//    $("button").click(function () {
+//        $.getJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&titles=Millard%20Fillmore&prop=revisions&rvprop=content'), function (result) {
+//            $.each(result, function (i, field) {
+//                $("div").append(field + " ");
+//            });
+//        };
+//    });
+//});
+
+//var text = JSON.stringify(text);
+
+var obj = JSON.parse('{"user":"JosephBenbihi","timestamp":"2013-11-17T07:37:34Z"}');
+
+alert(obj.timestamp);
+
+// Sandbox https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&list=abuselog&format=json&afllimit=10&aflprop=user%7Ctimestamp&pageids=19729548&redirects=
