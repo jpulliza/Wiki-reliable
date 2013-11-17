a70119dd44b7d7621d33092936802eaf4003680a
gerbal <grant.mclendon@unc.edu>
2013-11-16 23:47:18 -0500

    more working box of information

--- background.js
+++ background.js
@@ -1,16 +1,26 @@
-var box = document.getElementById( 'mw-content-text' );
+function putbox(document){
+	var box = document.getElementById('mw-content-text');
+	var table = document.createElement("table");
+	var newrow = document.createElement('tr');
+	var th = document.createElement('th');
+	table.className = "infobox vcard";
+	table.style.width = "22em";
+	th.colspan = "2";
+	th.className = "n";
+	th.style.textAlign = "center";
+	th.style.fontSize = "125%";
+	th.innerText = "hello world";
+	newrow.appendChild(th);
+	table.appendChild(newrow);
+	box.insertBefore(table, box.firstChild);
+}
 
-    var table = document.createElement("table");
-    var newrow = document.createElement( 'tr' );
-    var th = document.createElement('th');
-    table.className="infobox vcard";
-    table.style.width="22em";
-   th.colspan="2";
-    th.className="n";
-    th.style.text-align="center"; 
-    th.style.fontSize="125%";
-    th.innerText= "hello world";
-    newrow.appendChild(th);
-    table.appendChild(newrow);
-    box.insertBefore(table, box.firstChild);
-    
+// function runscript(document) {
+//   // No tabs or host permissions needed!
+//   console.log('putting box');
+//   chrome.tabs.executeScript({
+//     code: 'putbox(document)'
+//   });
+//};
+
+//chrome.tabs.onUpdated.addListener(runscript);
